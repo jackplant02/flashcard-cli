60a421952155d162f9e19a22c0947c2e4ce2d68f
Jack Plant <91396233+jackplant02@users.noreply.github.com>
2026-09-12 12:17:41 -0400

    Basic main strutcure

--- flashcard_program/main.py
+++ flashcard_program/main.py
@@ -1,5 +1,15 @@
 def main():
-    print("hello world")
+    while True:
+        print("\nWelcome back!\n")
+        entry = input("Please choose one of the following options: (a)dd a card, (s)tudy cards, or (q)uit: ")
+
+        if entry == "q":
+            break
+
+        else:
+            print("\nOops this program doesn't actually do anything yet lol.")
+            print("Bye.\n")
+            break
 
 if __name__ == "__main__":
     main()
