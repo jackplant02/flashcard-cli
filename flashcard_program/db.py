06153ce9d964296000a1dc9156265db68d0cb054
Jack Plant <91396233+jackplant02@users.noreply.github.com>
2026-09-12 12:23:14 -0400

    humble beginnings of sql database functions

--- flashcard_program/db.py
+++ flashcard_program/db.py
@@ -1,1 +1,9 @@
 import sqlite3
+
+DB_NAME = "flashcards.db"
+
+def init_db():
+    """Create the cards table it doesn't already exist."""
+    with sqlite3.connect(DB_NAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute
