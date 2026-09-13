c8b2db3fcfeb5574d3b74ad7688918a84aa2eb90
Jack Plant <91396233+jackplant02@users.noreply.github.com>
2026-09-12 20:17:55 -0400

    adding to db.py, slowly

--- flashcard_program/db.py
+++ flashcard_program/db.py
@@ -1,9 +1,27 @@
 import sqlite3
 
+# this is the filename where SQLite stores the database
 DB_NAME = "flashcards.db"
 
 def init_db():
     """Create the cards table it doesn't already exist."""
+
+    # opens a connection to 'flashcards.db' (creates the file if it doesn't exist)
+    # and closes it automatically when done
     with sqlite3.connect(DB_NAME) as conn:
+        # create a cursor object, which is used to send SQL commands to the database
         cursor = conn.cursor()
-        cursor.execute
+        # execute sql command (this one creates the 'cards' table, only if it doesn't exist)
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS cards (
+                id INTEGER PRIMARY KEY,
+                front TEXT NOT NULL,
+                back TEXT NOT NULL
+            )
+        """)
+        # Explicitly commit the changes 
+        conn.commit()
+
+        
+def add_card(front, back):
+    """Add a card to the database."""
